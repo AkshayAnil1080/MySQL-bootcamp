d875fb03824157e5edda5a50127499d4fb3fc211
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2021-03-24 15:07:14 +0530

    Create code.sql
    code

---
+++ Challenge 2/code.sql
@@ -0,0 +1,4 @@
+ CREATE TABLE people(first_name VARChAR(50) ,  last_name VARCHAR(50) , age INT);
+ INSERT INTO people(first_name, last_name, age)  VALUES('Linda', 'Belcher', 45) ,('Phillip', 'Frond', 38)  ,('Calvin', 'Fischoeder', 70);
+ DESC people;
+ DROP TABLE people;
